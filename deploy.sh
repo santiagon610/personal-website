c4ab1d2abb7d5d7f6a07e1a3f253fc5e35f4088f
Nicholas Santiago <nicholas.santiago@gmail.com>
2020-12-18 01:32:17 -0500

    add docs

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+if [ -z "$S3_PERSONAL_SITE" ]; then
+  echo "S3_PERSONAL_SITE environment var not set. Please set this variable and run again."
+  exit 1
+fi
+
+aws sts get-caller-identity | jq
+hugo
+aws s3 sync .public/ "$S3_PERSONAL_SITE"
