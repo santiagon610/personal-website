59bcb3f6d45b87a183de0a0be6d7f74c87cc38b2
Nicholas Santiago <nick@hlvpa.com>
2023-12-11 18:03:20 -0500

    cumulus/backlog#65: deploy site via hugo

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 set -e
 
-if [ -z "$S3_PERSONAL_SITE" ]; then
-  echo "S3_PERSONAL_SITE environment var not set. Please set this variable and run again."
-  exit 1
-fi
+DEPLOY_TARGET=${1:-production}
 
 aws sts get-caller-identity | jq
 hugo
-aws s3 sync ./public/ "s3://$S3_PERSONAL_SITE/"
+hugo deploy --target="${DEPLOY_TARGET}"
 rm -rf ./public/
